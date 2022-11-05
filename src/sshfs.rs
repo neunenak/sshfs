d04254c773cceb47a5399f6f98f11e356a57fd71
Greg Shuflin <greg@everydayimshuflin.com>
2022-11-04 21:42:12 -0700

    remove label_break_value

--- src/sshfs.rs
+++ src/sshfs.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
 #![register_tool(c2rust)]
-#![feature(extern_types, label_break_value, register_tool)]
+#![feature(extern_types, register_tool)]
 #[macro_use]
 extern crate c2rust_bitfields;
 use ::rust_output::*;
